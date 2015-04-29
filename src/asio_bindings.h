30c4b70772337e614c70d19c6b2b03dafef7269c
ironholds <ironholds@gmail.com>
2015-04-29 03:16:54 -0400

    start docs

--- src/asio_bindings.h
+++ src/asio_bindings.h
@@ -5,12 +5,35 @@ using namespace Rcpp;
 #ifndef __ASIO_BINDINGS__
 #define __ASIO_BINDINGS__
 
+/**
+ * A class containing the boost::asio bindings in use in iptools.
+ */
 class asio_bindings {
 
 private:
 
+  /**
+   * A consistent (well, instantiated once per run) io_service
+   * instance; used by hostname_to_dns/ip_to_dns functions, and
+   * (since it's reusable) we'll hopefully get a speed improvement
+   * out of only using one.
+   */
   boost::asio::io_service io_service;
 
+  /**
+   * A function for taking a hostname ("http://en.wikipedia.org")
+   * and converting it to the actual IP addresses it resolves to.
+   *
+   * @param hostname a hostname.
+   *
+   * @param resolver_ptr a reference to an asio resolver.
+   *
+   * @see single_ip_to_dns for the reverse operation, or
+   * multi_hostname_to_dns for the vectorised version.
+   *
+   * @return a vector containing the IP address(es) the hostname
+   * resolves to.
+   */
   std::vector < std::string > single_hostname_to_dns(std::string hostname, boost::asio::ip::tcp::resolver& resolver_ptr);
 
   std::vector < std::string > single_ip_to_dns(std::string ip_address, boost::asio::ip::tcp::resolver& resolver_ptr);
