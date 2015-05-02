ed00e4858be00bdee451a0b75b6198bf73adb18b
ironholds <ironholds@gmail.com>
2015-05-02 19:19:08 -0400

    expand introductory vignette

--- vignettes/introduction_to_iptools.R
+++ vignettes/introduction_to_iptools.R
@@ -1,7 +1,16 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ----eval=FALSE----------------------------------------------------------
+#  ips <- c("192.168.0.1","2607:f8b0:4006:80b::aaa","the next IP is also invalid","256.256.190.900")
+#  ip_classify(ips)
+#  [1] "IPv4"    "IPv6"    "Invalid" "Invalid"
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ----eval=FALSE----------------------------------------------------------
+#  #Dotted-decimal to numeric
+#  ips <- c("192.168.0.1","172.18.0.0","172.18.0.15")
+#  numeric_ips <- ip_to_numeric(ips)
+#  numeric_ips
+#  [1] 3232235521 2886860800 2886860815
+#  
+#  #And back again
+#  numeric_to_ip(numeric_ips)
+#  [1] "192.168.0.1" "172.18.0.0"  "172.18.0.15"
 
