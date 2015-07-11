9338c86f9e1d753e66e9afa735abec486d38ab5d
ironholds <ironholds@gmail.com>
2015-07-11 15:09:12 -0400

    remove Windows legacy elements

--- src/asio_bindings.h
+++ src/asio_bindings.h
@@ -1,11 +1,6 @@
 #include <Rcpp.h>
 #include <boost/asio.hpp>
 
-#ifdef _WIN32
-# include <windows.h>
-# include "windows/inet_v6defs.h"
-#endif
-
 using namespace Rcpp;
 
 #ifndef __ASIO_BINDINGS__
